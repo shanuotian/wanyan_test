12633094c867fd66b4a72d68cb205869a2c9c62c
吴杨 <illy623@163.com>
2022-02-13 22:15:22 +0800

    docker test 2022-02-13 22:15

--- src/main/java/org/spring/springboot/Application.java
+++ src/main/java/org/spring/springboot/Application.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // Spring Boot 应用的标识
 @SpringBootApplication
 public class Application {
+    
 
     public static void main(String[] args) {
         // 程序启动入口
